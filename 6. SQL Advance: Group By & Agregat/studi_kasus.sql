adf042d35caa4f300efcd1f66e72249efc017be2
projekbrillylutfan <projekbrillylutfan@gmail.com>
2025-07-30 17:07:41 +0700

    studi kasus udh selesai, belum di cek

--- 6. SQL Advance: Group By & Agregat/studi_kasus.sql
+++ 6. SQL Advance: Group By & Agregat/studi_kasus.sql
@@ -40,3 +40,54 @@ FROM `Students_Performance`
 WHERE
     gender = "female"
     and test_preparation_course = "completed"
+
+--4
+
+SELECT * FROM `Students_Performance`
+
+SELECT
+    parental_level_of_education,
+    AVG(writing_score) as writing_score_rata_rata
+FROM `Students_Performance`
+WHERE
+    parental_level_of_education = "high school"
+    OR parental_level_of_education = "some high school"
+GROUP BY
+    parental_level_of_education
+
+--5
+SELECT * FROM `Students_Performance`
+
+SELECT gender,
+race_or_ethnicity,
+test_preparation_course,
+AVG(math_score) AS avg_math_score,
+AVG(reading_score) AS avg_reading_score,
+AVG(writing_score) AS avg_writing_score
+FROM `Students_Performance`
+GROUP BY gender,
+race_or_ethnicity,
+test_preparation_course
+ORDER BY gender,
+race_or_ethnicity,
+test_preparation_course;
+
+--6
+
+SELECT 
+  gender,
+  race_or_ethnicity,
+  test_preparation_course,
+  AVG(math_score) AS avg_math_score,
+  AVG(reading_score) AS avg_reading_score,
+  AVG(writing_score) AS avg_writing_score
+FROM Students_Performance
+GROUP BY 
+  gender,
+  race_or_ethnicity,
+  test_preparation_course
+HAVING avg_math_score > 70
+ORDER BY 
+  gender,
+  race_or_ethnicity,
+  test_preparation_course;
